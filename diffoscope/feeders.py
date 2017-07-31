0cdafa37bd8cab63991ea18cdf16361d6516d9f3
Ximin Luo <infinity0@debian.org>
2017-07-31 17:04:03 +0200

    Force a flush when writing output to diff. (Closes: #870049)

--- diffoscope/feeders.py
+++ diffoscope/feeders.py
@@ -51,6 +51,9 @@ def from_raw_reader(in_file, filter=lambda buf: buf):
 
             if line_count < max_lines:
                 out_file.write(out)
+                # very long lines can sometimes interact negatively with python
+                # buffering; force a flush here to avoid this, see #870049
+                out_file.flush()
             end_nl = buf[-1] == '\n'
 
         if h is not None and line_count >= max_lines:
